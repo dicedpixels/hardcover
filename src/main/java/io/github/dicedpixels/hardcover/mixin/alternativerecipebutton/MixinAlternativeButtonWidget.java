13f17f61f75f51286f32e86c87cec18c2421b680
dicedpixels <dicedpixels@hotmail.com>
2023-05-24 22:30:32 +0530

    refactor: more code style changes

--- src/main/java/io/github/dicedpixels/hardcover/mixin/alternativerecipebutton/MixinAlternativeButtonWidget.java
+++ src/main/java/io/github/dicedpixels/hardcover/mixin/alternativerecipebutton/MixinAlternativeButtonWidget.java
@@ -1,7 +1,21 @@
 package io.github.dicedpixels.hardcover.mixin.alternativerecipebutton;
 
+import static java.util.stream.Collectors.groupingBy;
+import static java.util.stream.Collectors.toMap;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
 import com.google.common.collect.Lists;
 import io.github.dicedpixels.hardcover.Hardcover;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.DrawContext;
@@ -17,21 +31,6 @@ import net.minecraft.recipe.Recipe;
 import net.minecraft.recipe.RecipeGridAligner;
 import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
-
-import org.spongepowered.asm.mixin.Final;
-import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-
-import java.util.Arrays;
-import java.util.List;
-import java.util.Map;
-import java.util.Optional;
-
-import static java.util.stream.Collectors.groupingBy;
-import static java.util.stream.Collectors.toMap;
 
 @Mixin(AlternativeButtonWidget.class)
 abstract class MixinAlternativeButtonWidget extends ClickableWidget implements RecipeGridAligner<Ingredient> {
@@ -52,20 +51,6 @@ abstract class MixinAlternativeButtonWidget extends ClickableWidget implements R
 
 	public MixinAlternativeButtonWidget(int x, int y, int width, int height, Text message) {
 		super(x, y, width, height, message);
-	}
-
-	private static List<Text> hardcover$getIngredientList(Recipe<?> recipe) {
-		Map<Item, List<Ingredient>> collect = recipe.getIngredients().stream().filter(i -> !i.isEmpty()).collect(groupingBy(g -> {
-			Optional<ItemStack> stack = Arrays.stream(g.getMatchingStacks()).findFirst();
-			return stack.isPresent() ? stack.get().getItem() : Items.AIR;
-		}));
-		Map<String, Integer> collected = collect.entrySet().stream().filter(e -> e.getKey() != Items.AIR).collect(toMap(e -> e.getKey().getTranslationKey(), e -> e.getValue().size()));
-		List<Text> text = Lists.newArrayList();
-		int i = 0;
-		for (Map.Entry<String, Integer> entry : collected.entrySet()) {
-			text.add(Text.translatable(entry.getKey()).append(" x").append(String.valueOf(entry.getValue())));
-		}
-		return text;
 	}
 
 	@Inject(method = "renderButton", at = @At("HEAD"), cancellable = true)
@@ -90,5 +75,19 @@ abstract class MixinAlternativeButtonWidget extends ClickableWidget implements R
 			context.getMatrices().pop();
 			ci.cancel();
 		}
+	}
+
+	private static List<Text> hardcover$getIngredientList(Recipe<?> recipe) {
+		Map<Item, List<Ingredient>> collect = recipe.getIngredients().stream().filter(i -> !i.isEmpty()).collect(groupingBy(g -> {
+			Optional<ItemStack> stack = Arrays.stream(g.getMatchingStacks()).findFirst();
+			return stack.isPresent() ? stack.get().getItem() : Items.AIR;
+		}));
+		Map<String, Integer> collected = collect.entrySet().stream().filter(e -> e.getKey() != Items.AIR).collect(toMap(e -> e.getKey().getTranslationKey(), e -> e.getValue().size()));
+		List<Text> text = Lists.newArrayList();
+		int i = 0;
+		for (Map.Entry<String, Integer> entry : collected.entrySet()) {
+			text.add(Text.translatable(entry.getKey()).append(" x").append(String.valueOf(entry.getValue())));
+		}
+		return text;
 	}
 }
