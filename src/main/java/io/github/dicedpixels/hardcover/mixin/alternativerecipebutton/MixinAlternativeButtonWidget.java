5b652082fdc1c94a4b1ba6f61db381c747093e42
dicedpixels <dicedpixels@hotmail.com>
2023-05-24 22:15:02 +0530

    fix: tooltip rendering in alternative button widget

--- src/main/java/io/github/dicedpixels/hardcover/mixin/alternativerecipebutton/MixinAlternativeButtonWidget.java
+++ src/main/java/io/github/dicedpixels/hardcover/mixin/alternativerecipebutton/MixinAlternativeButtonWidget.java
@@ -1,27 +1,13 @@
 package io.github.dicedpixels.hardcover.mixin.alternativerecipebutton;
 
-import static java.util.stream.Collectors.groupingBy;
-import static java.util.stream.Collectors.toMap;
-
-import java.util.Arrays;
-import java.util.List;
-import java.util.Map;
-import java.util.Optional;
-
+import com.google.common.collect.Lists;
 import io.github.dicedpixels.hardcover.Hardcover;
-import org.spongepowered.asm.mixin.Final;
-import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.gui.screen.recipebook.RecipeAlternativesWidget;
 import net.minecraft.client.gui.screen.recipebook.RecipeAlternativesWidget.AlternativeButtonWidget;
 import net.minecraft.client.gui.screen.recipebook.RecipeAlternativesWidget.AlternativeButtonWidget.InputSlot;
-import net.minecraft.client.gui.tooltip.Tooltip;
 import net.minecraft.client.gui.widget.ClickableWidget;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
@@ -29,9 +15,23 @@ import net.minecraft.item.Items;
 import net.minecraft.recipe.Ingredient;
 import net.minecraft.recipe.Recipe;
 import net.minecraft.recipe.RecipeGridAligner;
-import net.minecraft.text.MutableText;
 import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
+
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+import static java.util.stream.Collectors.groupingBy;
+import static java.util.stream.Collectors.toMap;
 
 @Mixin(AlternativeButtonWidget.class)
 abstract class MixinAlternativeButtonWidget extends ClickableWidget implements RecipeGridAligner<Ingredient> {
@@ -54,26 +54,16 @@ abstract class MixinAlternativeButtonWidget extends ClickableWidget implements R
 		super(x, y, width, height, message);
 	}
 
-	@Inject(method = "<init>", at = @At("TAIL"))
-	private void hardcover$MixinAlternativeButtonWidgetConstructor(RecipeAlternativesWidget recipeAlternativesWidget, int x, int y, Recipe<?> recipe, boolean craftable, CallbackInfo ci) {
-		setTooltip(Tooltip.of(hardcover$getIngredientList(recipe)));
-	}
-
-	private static Text hardcover$getIngredientList(Recipe<?> recipe) {
+	private static List<Text> hardcover$getIngredientList(Recipe<?> recipe) {
 		Map<Item, List<Ingredient>> collect = recipe.getIngredients().stream().filter(i -> !i.isEmpty()).collect(groupingBy(g -> {
 			Optional<ItemStack> stack = Arrays.stream(g.getMatchingStacks()).findFirst();
 			return stack.isPresent() ? stack.get().getItem() : Items.AIR;
 		}));
 		Map<String, Integer> collected = collect.entrySet().stream().filter(e -> e.getKey() != Items.AIR).collect(toMap(e -> e.getKey().getTranslationKey(), e -> e.getValue().size()));
-		MutableText text = Text.empty();
+		List<Text> text = Lists.newArrayList();
 		int i = 0;
 		for (Map.Entry<String, Integer> entry : collected.entrySet()) {
-			if (i == collected.size() - 1) {
-				text.append(Text.translatable(entry.getKey())).append(" x").append(String.valueOf(entry.getValue()));
-				continue;
-			}
-			text.append(Text.translatable(entry.getKey())).append(" x").append(String.valueOf(entry.getValue())).append("\n");
-			i++;
+			text.add(Text.translatable(entry.getKey()).append(" x").append(String.valueOf(entry.getValue())));
 		}
 		return text;
 	}
@@ -88,6 +78,7 @@ abstract class MixinAlternativeButtonWidget extends ClickableWidget implements R
 			}
 			if (isHovered()) {
 				v += 26;
+				context.drawTooltip(client.textRenderer, hardcover$getIngredientList(recipe), mouseX, mouseY);
 			}
 			if (Hardcover.CONFIG.darkMode) {
 				v += 52;
