3e4640a387d41110521b0014c128f501a1a3c574
dicedpixels <dicedpixels@hotmail.com>
2024-11-03 15:29:10 +0530

    fix recipe button hiding

--- src/main/java/xyz/dicedpixels/hardcover/Initializer.java
+++ src/main/java/xyz/dicedpixels/hardcover/Initializer.java
@@ -1,8 +1,28 @@
 package xyz.dicedpixels.hardcover;
 
+import net.minecraft.client.gui.screen.recipebook.RecipeBookProvider;
+import net.minecraft.client.gui.screen.recipebook.RecipeBookWidget;
+import net.minecraft.client.gui.widget.TexturedButtonWidget;
+
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.client.screen.v1.ScreenEvents;
+import net.fabricmc.fabric.api.client.screen.v1.Screens;
+
+import xyz.dicedpixels.hardcover.mixin.accessors.TexturedButtonWidgetAccessor;
 
 public class Initializer implements ModInitializer {
     @Override
-    public void onInitialize() {}
+    public void onInitialize() {
+        ScreenEvents.AFTER_INIT.register((minecraftClient, screen, scaledWidth, scaledHeight) -> {
+            if (!Config.instance().recipeBook && screen instanceof RecipeBookProvider) {
+                Screens.getButtons(screen).removeIf(button -> {
+                    if (button instanceof TexturedButtonWidget) {
+                        return ((TexturedButtonWidgetAccessor) button).hardcover$textures() == RecipeBookWidget.BUTTON_TEXTURES;
+                    }
+
+                    return false;
+                });
+            }
+        });
+    }
 }
